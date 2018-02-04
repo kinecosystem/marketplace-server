e9c31ae3fc5be7987847233948ee4cc5b41a4c6f
Doody Parizada <doody.parizada@gmail.com>
2018-02-04 14:01:06 +0200

    add mock offers

--- scripts/src/services/offers.ts
+++ scripts/src/services/offers.ts
@@ -53,10 +53,45 @@ export type OfferList = {
 }
 
 export const getOffers = async (options): Promise<ServiceResult<OfferList>> => {
+
+	const earn = [
+		"earn_offer1.png", 
+		"earn_offer2.png", 
+		"earn_offer3.png", 
+		"earn_offer4.png", 
+		"earn_offer5.png", 
+		];
+	const spend = [
+		"spend_offer1.png",
+		"spend_offer2.png",
+		"spend_offer3.png",
+		"spend_offer4.png",
+		"spend_offer5.png",
+	];
+	const IMAGE_BASE = "https://s3.amazonaws.com/kinmarketplace-assets/version1/";
+	const offers =  
+		earn.map<Offer>(img => ({
+			id: img,
+			title: "Answer a poll",
+			description: "Tell us about yourself",
+			image: IMAGE_BASE + img,
+			amount: 4000,
+			content: {} as Coupon,
+			offer_type: "earn",
+		})).concat(spend.map<Offer>(img => ({
+			id: img,
+			title: "Gift Card",
+			description: "$10 gift card",
+			image: IMAGE_BASE + img,
+			amount: 8000,
+			content: {} as Coupon,
+			offer_type: "spend",
+		})));
+
 	return {
 		code: 200,
 		data: {
-			offers: []
+			offers
 		}
 	};
 };

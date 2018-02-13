804ea338726a04c561212c35e8bcc0d7982d2303
Doody Parizada <doody.parizada@gmail.com>
2018-02-13 15:15:10 +0200

    fix styling issues

--- scripts/src/services/users.ts
+++ scripts/src/services/users.ts
@@ -5,7 +5,7 @@ type JWTClaims = {
 	exp: number; // expiration
 	iat: number; // issued at
 	sub: string; // subject
-}
+};
 
 type JWTContent = {
 	header: {
@@ -18,23 +18,25 @@ type JWTContent = {
 		user_id: string;
 	};
 	signature: string;
-}
+};
 
 function getApplicationPublicKey(applicationId: string, keyId: string) {
 	// return the public key for the given application.
 	// an application might have multiple keys. each key identified by key_id.
 
 	const publicKeys = {
-		fancy: {1: "sdfnksdjfhlskjfhksdf", 2: "23423423423423"},
+		fancy: { 1: "sdfnksdjfhlskjfhksdf", 2: "23423423423423" },
 		kik: {one: "-----BEGIN PUBLIC KEY-----\n" +
-			"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQAB\n" +
+			"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4ar" +
+			"WNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW+j" +
+			"K+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQAB\n" +
 			"-----END PUBLIC KEY-----"}};
 
 	return publicKeys[applicationId][keyId];
 }
 
 export function validateJWT(jwt: string): {userId: string, appId: string} {
-	const decoded = jsonwebtoken.decode(jwt, {complete: true}) as JWTContent;
+	const decoded = jsonwebtoken.decode(jwt, { complete: true }) as JWTContent;
 	const publicKey = getApplicationPublicKey(decoded.payload.iss, decoded.header.key);
 
 	jsonwebtoken.verify(jwt, publicKey);

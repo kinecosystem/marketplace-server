5e0eca396d562ce27e044112ef65e71818782d2d
Doody Parizada <doody.parizada@gmail.com>
2018-02-14 13:35:58 +0200

    re implement user reg flow

--- scripts/src/models/users.ts
+++ scripts/src/models/users.ts
@@ -1,34 +1,24 @@
 import { Column, Entity, PrimaryColumn } from "typeorm";
 
 import { CreationDateModel, Model, Register } from "./index";
-import { IdPrefix } from "../utils";
 
 @Entity()
 @Register
 export class User extends CreationDateModel {
 	@Column({ name: "app_id" })
-	private _appId: string;
+	public appId: string;
 
 	@Column({ name: "app_user_id" })
-	private _appUserId: string;
+	public appUserId: string;
+
+	@Column({ name: "wallet_address" })
+	public walletAddress: string;
 
 	@Column({ name: "activated_date" })
-	private _activatedDate: Date;
+	public activatedDate: Date;
 
-	public constructor() {
-		super(IdPrefix.User);
-	}
-
-	public get appId(): string {
-		return this._appId;
-	}
-
-	public get appUserId(): string {
-		return this._appUserId;
-	}
-
-	public get activatedDate(): Date {
-		return this._activatedDate;
+	public get activated(): boolean {
+		return !!this.activatedDate;
 	}
 }
 
@@ -36,63 +26,27 @@ export class User extends CreationDateModel {
 @Register
 export class AuthToken extends CreationDateModel {
 	@Column({ name: "activated_date" })
-	private _expireDate: Date;
+	public expireDate: Date;
 
 	@Column({ name: "device_id" })
-	private _deviceId: string;
+	public deviceId: string;
 
-	@Column({ name: "token", unique: true })
-	private _token: string;
+	@Column({ name: "token" })
+	public token: string;
 
 	@Column({ name: "user_id" })
-	private _userId: string;
+	public userId: string;
 
 	@Column({ name: "valid" })
-	private _valid: boolean;
-
-	public constructor() {
-		super();
-	}
-
-	public get expireDate(): Date {
-		return this._expireDate;
-	}
-
-	public get deviceId(): string {
-		return this._deviceId;
-	}
-
-	public get token(): string {
-		return this._token;
-	}
-
-	public get userId(): string {
-		return this._token;
-	}
-
-	public get valid(): boolean {
-		return this._valid;
-	}
+	public valid: boolean;
 }
 
 @Entity()
 @Register
 export class Application extends CreationDateModel {
 	@Column({ name: "name" })
-	private _name: string;
+	public name: string;
 
 	@Column({ name: "jwt_public_key" })
-	private _jwtPublicKey: string;
-
-	public constructor() {
-		super(IdPrefix.App);
-	}
-
-	public get name(): string {
-		return this._name;
-	}
-
-	public get jwtPublicKey(): string {
-		return this._jwtPublicKey;
-	}
+	public jwtPublicKey: string;
 }

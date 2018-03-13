719b0f043b5ab02297f6a7c3995894339f11ec2a
Doody Parizada <doody.parizada@gmail.com>
2018-03-13 11:26:24 +0200

    pr fix

--- tests/src/services/index.spec.ts
+++ tests/src/services/index.spec.ts
@@ -5,7 +5,7 @@ import { app } from "../../../scripts/bin/app";
 describe("general api checks", async () => {
 	test("unknown api endpoints should return 404", async () => {
 		await mock(app)
-		.get("/v1/no_such_page")
-		.expect(404);
+			.get("/v1/no_such_page")
+			.expect(404);
 	});
 });

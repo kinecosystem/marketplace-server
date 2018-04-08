85e6847d0f96511c98930f658a039357fea7aa20
nitzantomer <nitzan.tomer@gmail.com>
2018-04-09 01:03:41 +0300

    created the more generic getOrder instead of the very specific getNonOpen

--- scripts/src/models/orders.ts
+++ scripts/src/models/orders.ts
@@ -24,12 +24,15 @@ export type OrderError = {
 @Register
 @Initializer("id", () => generateId(IdPrefix.Transaction))
 export class Order extends CreationDateModel {
+	public static getOrder(orderId: string, status?: OpenOrderStatus | "!opened" | "!completed" | "!failed" | "!pending") {
+		const query = Order.createQueryBuilder()
+			.where("id = :orderId", { orderId });
 
-	public static getNonOpen(orderId: string): Promise<Order | undefined> {
-		return Order.createQueryBuilder()
-			.where("id = :orderId", { orderId })
-			.andWhere("status != :status", { status: "opened" })
-			.getOne();
+		if (status) {
+			query.andWhere("status :eq :status", { eq: status.startsWith("!"), status: "opened" });
+		}
+
+		return query.getOne();
 	}
 
 	public static getAllNonOpen(userId: string, limit: number): Promise<Order[]> {

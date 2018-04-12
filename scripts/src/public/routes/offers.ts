b1a715beea654550315501bbb29e73e9c6b32db2
nitzantomer <nitzan.tomer@gmail.com>
2018-04-12 14:58:05 +0300

    moved createMarketplaceOrder to routes/orders

--- scripts/src/public/routes/offers.ts
+++ scripts/src/public/routes/offers.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 import { OfferType } from "../../models/offers";
 
 import { getOffers as getOffersService } from "../services/offers";
-import { createMarketplaceOrder as createMarketplaceOrderService } from "../services/orders";
 
 /**
  * Return a list of offers
@@ -21,29 +20,3 @@ export const getOffers = async function(req: GetOffersRequest, res: Response, ne
 		next(err);
 	}
 } as RequestHandler;
-
-/**
- * create an order for an offer
- *
- * with GlobalLock(req.context.userId + req.context.offerId):
- *   // did the transaction
- *   existing_transactions = Transaction.find({offerId, userId})
- *   offer = Offer.findOneById(offerId)
- *   if len(existing_transactions) >= offer.user_cap:
- *     throw NoCapLeft("for user")
- *   existing_order = OpenOrder.find({userId: userId, offerId: offerId})
- *   if existing_order && !existing_order.is_expired():
- *     return existing_order
- *   // no existing order:
- *   locked_cap = len(OpenOrders.find({offerId}))
- *   left_cap = offer.cap - offer.used - locked_cap
- *   if not left_cap:
- *     throw NoCapLeft("depleted resource")
- *   // create a new order
- *   order = OpenOrders.create(offerId, userId) // this adds to the locked_cap
- *   return order
- */
-export const createMarketplaceOrder = async function(req: Request, res: Response) {
-	const order = await createMarketplaceOrderService(req.params.offer_id, req.context.user!.id, req.logger);
-	res.status(201).send(order);
-} as any as RequestHandler;
--- scripts/src/public/routes/orders.ts
+++ scripts/src/public/routes/orders.ts
@@ -5,8 +5,34 @@ import {
 	getOrder as getOrderService,
 	getOrderHistory as getOrderHistoryService,
 	submitOrder as submitOrderService,
-	OrderList,
+	OrderList, createMarketplaceOrder as createMarketplaceOrderService,
 } from "../services/orders";
+
+/**
+ * create an order for an offer
+ *
+ * with GlobalLock(req.context.userId + req.context.offerId):
+ *   // did the transaction
+ *   existing_transactions = Transaction.find({offerId, userId})
+ *   offer = Offer.findOneById(offerId)
+ *   if len(existing_transactions) >= offer.user_cap:
+ *     throw NoCapLeft("for user")
+ *   existing_order = OpenOrder.find({userId: userId, offerId: offerId})
+ *   if existing_order && !existing_order.is_expired():
+ *     return existing_order
+ *   // no existing order:
+ *   locked_cap = len(OpenOrders.find({offerId}))
+ *   left_cap = offer.cap - offer.used - locked_cap
+ *   if not left_cap:
+ *     throw NoCapLeft("depleted resource")
+ *   // create a new order
+ *   order = OpenOrders.create(offerId, userId) // this adds to the locked_cap
+ *   return order
+ */
+export const createMarketplaceOrder = async function(req: Request, res: Response) {
+	const order = await createMarketplaceOrderService(req.params.offer_id, req.context.user!.id, req.logger);
+	res.status(201).send(order);
+} as any as RequestHandler;
 
 /**
  * get an order

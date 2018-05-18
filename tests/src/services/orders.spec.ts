2a1630a0dbea1ae560c77b72ebf041a4f29b7914
Doody Parizada <doody.parizada@gmail.com>
2018-05-18 21:58:18 +0300

    add test for kik special case for jwt es256

--- tests/src/services/orders.spec.ts
+++ tests/src/services/orders.spec.ts
@@ -2,15 +2,18 @@ import * as moment from "moment";
 
 import { random } from "../../../scripts/bin/utils";
 import { User } from "../../../scripts/bin/models/users";
-import { Order } from "../../../scripts/bin/models/orders";
-import { Offer } from "../../../scripts/bin/models/offers";
+import { Order, ExternalOrder } from "../../../scripts/bin/models/orders";
+import { Offer, JWTValue } from "../../../scripts/bin/models/offers";
 import * as payment from "../../../scripts/bin/public/services/payment";
 import { getOffers } from "../../../scripts/bin/public/services/offers";
 import { getDefaultLogger, initLogger } from "../../../scripts/bin/logging";
 import { init as initModels, close as closeModels } from "../../../scripts/bin/models/index";
 import { createMarketplaceOrder, submitOrder } from "../../../scripts/bin/public/services/orders";
+import { JWTContent } from "../../../scripts/bin/public/jwt";
 
 import * as helpers from "../helpers";
+import * as jsonwebtoken from "jsonwebtoken";
+import * as expect from "expect";
 
 describe("test orders", async () => {
 	jest.setTimeout(20000);
@@ -60,7 +63,7 @@ describe("test orders", async () => {
 	});
 
 	test("return getOrder reduces cap", async () => {
-		(payment.payTo as any) = function() {
+		(payment.payTo as any) = function () {
 			return 1;
 		}; // XXX use a patching library
 
@@ -75,6 +78,39 @@ describe("test orders", async () => {
 
 		const offers2 = await getOffers(user.id, user.appId, {}, getDefaultLogger());
 		expect(offers2.offers.length).toBeLessThan(offers.offers.length);
+	});
+
+	test("payment jwt for kik is es256", async () => {
+		async function getPaymentJWT(appId: string) {
+			const user: User = await helpers.createUser(appId);
+			const order = ExternalOrder.new({
+				userId: user.id,
+				offerId: "offer",
+				amount: 1,
+				type: "spend",
+				status: "opened",
+				meta: {},
+				blockchainData: {
+					sender_address: "sender",
+					recipient_address: "recipient"
+				}
+			});
+			await order.save();
+			await helpers.completePayment(order.id);
+
+			const completedOrder = await Order.getOne(order.id);
+			expect(completedOrder.value.type).toBe("payment_confirmation");
+			return jsonwebtoken.decode(
+				(completedOrder.value as JWTValue).jwt, { complete: true }
+			) as JWTContent<any, "payment_confirmation">;
+		}
+		const kikJWT = await getPaymentJWT("kik");
+		expect(kikJWT.header.alg.toLowerCase()).toBe("es256");
+		expect(kikJWT.header.kid).toBe("es256_0");
+
+		const smplJWT = await getPaymentJWT("smpl");
+		expect(smplJWT.header.alg.toLowerCase()).not.toBe("es256");
+		expect(smplJWT.header.kid).not.toBe("es256_0");
 	});
 
 	test("expiration on openOrder is 10 minutes", async () => {

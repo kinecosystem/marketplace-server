93e952c97e16c81ac5483454b8423cdecf244567
Doody Parizada <doody.parizada@gmail.com>
2018-06-07 14:47:09 +0300

    write public and private keys to different dirs

--- operational/get_keys.sh
+++ operational/get_keys.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+## reads all public/ private keys starting with 'prod-kin' and ending with 'pem' and writes them to a dir, removing the 'prod-' part from the filename
 
 if [ $# == 1 ] && [ $1 == private ]
 then
     PUBLIC_FLAG=
+    DIR=priv_keys
 else
     PUBLIC_FLAG=-v
+    DIR=pub_keys
 fi
 
 keys=`aws --region=us-east-1 ssm describe-parameters | jq -r '.Parameters[].Name' | grep 'prod-kin-.*pem' | grep $PUBLIC_FLAG -- '-priv.pem'`
 
 for key in $keys; do
     # write the keys with the same name as the parameter name stripping off the "prod-" prefix
-    aws --region=us-east-1 ssm get-parameters --names $key --with-decryption | jq -r '.Parameters[] | select(.Name == "'$key'") | .Value' > /opt/marketplace-server/keys/${key:5}
+    mkdir -p $DIR
+    aws --region=us-east-1 ssm get-parameters --names $key --with-decryption | jq -r '.Parameters[] | select(.Name == "'$key'") | .Value' > /opt/marketplace-server/$DIR/${key:5}
 done
+
+echo wrote keys to $DIR

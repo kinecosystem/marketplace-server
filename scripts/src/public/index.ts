6c5e1c0430a714c2b96b3b07a15894622831bd19
Doody Parizada <doody.parizada@gmail.com>
2018-11-27 15:56:32 -0800

    implement clustering in nodejs

--- scripts/src/public/index.ts
+++ scripts/src/public/index.ts
@@ -1,20 +1,41 @@
-import * as http from "http";
+// for cluster master
+import * as cluster from "cluster";
+import * as os from "os";
 
+// for cluster workers
+import * as http from "http";
 import { getConfig } from "./config";
-import { initLogger } from "../logging";
 
 const config = getConfig();
 import { app, init } from "./app";
 
 import { onError, onListening } from "../server";
 
-const server = http.createServer(app);
+if (cluster.isMaster) {
+	// Count the machine's CPUs
+	const cpuCount = os.cpus().length;
 
-init().then(() => {
-	server.listen(config.port);
-	server.on("error", onError);
-	server.on("listening", onListening(server));
-}).catch(e => {
-	console.log("failed to start server: " + e);
-	process.exit(1);
-});
+	// Create a worker for each CPU
+	os.cpus().forEach(() => cluster.fork());
+
+	// Listen for dying workers
+	cluster.on("exit", function(worker) {
+
+		// Replace the dead worker, we're not sentimental
+		console.log(`Worker ${worker.id} died`);
+		cluster.fork();
+
+	});
+} else {
+	init().then(() => {
+		const server = http.createServer(app);
+		server.listen(config.port);
+		server.on("error", onError);
+		server.on("listening", onListening(server));
+		console.log(`Worker ${cluster.worker.id} running`);
+
+	}).catch(e => {
+		console.log(`Worker ${cluster.worker.id} failed: failed to start server: ${e}`);
+		process.exit(1);
+	});
+}

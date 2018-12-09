5280b461562ce5a7ea9ba726182e0266a0b18ca5
Doody Parizada <doody.parizada@gmail.com>
2018-12-09 16:31:56 +0200

    PR fix

--- scripts/src/public/index.ts
+++ scripts/src/public/index.ts
@@ -14,17 +14,13 @@ import { onError, onListening } from "../server";
 if (cluster.isMaster) {
 	// Count the machine's CPUs
 	const cpuCount = os.cpus().length;
-
 	// Create a worker for each CPU
 	os.cpus().forEach(() => cluster.fork());
-
 	// Listen for dying workers
-	cluster.on("exit", function(worker) {
-
+	cluster.on("exit", worker => {
 		// Replace the dead worker, we're not sentimental
 		console.log(`Worker ${worker.id} died`);
 		cluster.fork();
-
 	});
 } else {
 	init().then(() => {
@@ -33,7 +29,6 @@ if (cluster.isMaster) {
 		server.on("error", onError);
 		server.on("listening", onListening(server));
 		console.log(`Worker ${cluster.worker.id} running`);
-
 	}).catch(e => {
 		console.log(`Worker ${cluster.worker.id} failed: failed to start server: ${e}`);
 		process.exit(1);

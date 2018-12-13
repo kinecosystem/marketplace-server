fd7d7116fce012c8d8bebcb784aee2a36288b255
Doody Parizada <doody.parizada@gmail.com>
2018-12-13 17:58:29 +0200

    install context

--- scripts/src/public/middleware.ts
+++ scripts/src/public/middleware.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import * as bearerToken from "express-bearer-token";
+import * as httpContext from "express-http-context";
+// const httpContext = require("express-bearer-token");
 
 import { init as baseInit, requestLogger, logRequest, reportMetrics } from "../middleware";
 
@@ -7,7 +9,7 @@ export * from "../middleware";
 
 export function init(app: express.Express) {
 	baseInit();
-
+	app.use(httpContext.middleware as express.RequestHandler);
 	app.use(requestLogger);
 	app.use(bearerToken());
 	app.use(logRequest);

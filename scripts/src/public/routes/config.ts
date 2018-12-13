f8350e2edeeba557ea25117a8fcb5a6dbc2cd8f2
Doody Parizada <doody.parizada@gmail.com>
2018-12-13 19:14:31 +0200

    use log dynamically

--- scripts/src/public/routes/config.ts
+++ scripts/src/public/routes/config.ts
@@ -2,7 +2,7 @@ import { KeyMap } from "../../utils/utils";
 import { getConfig } from "../config";
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import { BlockchainConfig, getBlockchainConfig } from "../services/payment";
-import { getDefaultLogger } from "../../logging";
+import { getDefaultLogger as log } from "../../logging";
 import { getJwtKeys } from "../services/internal_service";
 
 const CONFIG = getConfig();
@@ -11,7 +11,7 @@ let JWT_KEYS: KeyMap;
 let BLOCKCHAIN: BlockchainConfig;
 
 export async function init() {
-	BLOCKCHAIN = await getBlockchainConfig(getDefaultLogger());
+	BLOCKCHAIN = await getBlockchainConfig(log());
 	JWT_KEYS = await getJwtKeys();
 }
 
@@ -27,7 +27,7 @@ export type ConfigResponse = {
 export const getConfigHandler = async function(req: Request, res: Response, next: NextFunction) {
 	const data: ConfigResponse = {
 		jwt_keys: await getJwtKeys(),
-		blockchain: await getBlockchainConfig(getDefaultLogger()),
+		blockchain: await getBlockchainConfig(log()),
 		bi_service: CONFIG.bi_service,
 		webview: CONFIG.webview,
 		environment_name: CONFIG.environment_name,
--- scripts/src/server.ts
+++ scripts/src/server.ts
@@ -3,15 +3,14 @@ import { Server } from "http";
 import * as metrics from "./metrics";
 import { getConfig } from "./config";
 import { ServerError } from "./utils/utils";
-import { getDefaultLogger } from "./logging";
+import { getDefaultLogger as log } from "./logging";
 
 const config = getConfig();
-const logger = getDefaultLogger();
 
 function cleanup(server: Server) {
-	logger.info("Shutting down");
+	log().info("Shutting down");
 	server.close(() => {
-		logger.info("Done, have a great day!");
+		log().info("Done, have a great day!");
 		process.exit(0);
 	});
 }
@@ -27,11 +26,11 @@ export function onError(error: ServerError) {
 	// handle specific listen errors with friendly messages
 	switch (error.code) {
 		case "EACCES":
-			logger.error(`${ config.port } requires elevated privileges`);
+			log().error(`${ config.port } requires elevated privileges`);
 			process.exit(1);
 			break;
 		case "EADDRINUSE":
-			logger.error(`${ config.port } is already in use`);
+			log().error(`${ config.port } is already in use`);
 			process.exit(1);
 			break;
 		default:
@@ -48,7 +47,7 @@ export function onListening(server: Server) {
 		const handler = cleanup.bind(null, server);
 		process.on("SIGINT", handler);
 		process.on("SIGTERM", handler);
-		logger.debug(`Listening on ${ addr.port }`);
+		log().debug(`Listening on ${ addr.port }`);
 	};
 }
 

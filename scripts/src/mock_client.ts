9cb12d879125ac5ac39e75f8f1481a0bfdaab0a2
Doody Parizada <doody.parizada@gmail.com>
2018-12-16 12:49:21 +0200

    Revert "Revert "sumCorrectQuizAnswers logic adapted to work with answers indices, moc… (#214)""
    This reverts commit dd0c80f327cd00e0190c1c957ce25bcf920e95bc.

--- scripts/src/mock_client.ts
+++ scripts/src/mock_client.ts
@@ -172,7 +172,7 @@ async function earnPollFlow() {
 		for (const page of poll.pages.slice(0, poll.pages.length - 1)) {
 			const p = (page as PollPage);
 			const choice = randomInteger(0, p.question.answers.length);
-			answers[p.question.id] = p.question.answers[choice];
+			answers[p.question.id] = choice;
 		}
 		return answers;
 	}
@@ -231,7 +231,7 @@ async function earnQuizFlow() {
 			if (choice === p.rightAnswer) {
 				sum += p.amount;
 			}
-			answers[p.question.id] = choice > 0 ? p.question.answers[choice - 1] : "";
+			answers[p.question.id] = choice;
 		}
 		return [answers, sum || 1]; // server will give 1 kin for failed quizes
 	}

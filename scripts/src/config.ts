ee23f74dfdd72961f6c6d0dafaa4b896df20a614
Doody Parizada <doody.parizada@gmail.com>
2019-01-06 18:12:59 +0200

    add statsd config

--- scripts/src/config.ts
+++ scripts/src/config.ts
@@ -38,12 +38,12 @@ export function init(filePath: string) {
 		return;
 	}
 	config = require(path(filePath!));
-	config.port = parseInt(process.env.APP_PORT || "", 10) || config.port;
+	config.port = Number(process.env.APP_PORT || "") || config.port;
 	config.host = process.env.APP_HOST || config.host;
 	(config.db as any).type = process.env.APP_DB_TYPE || config.db.type;
 	(config.db as any).username = process.env.APP_DB_USERNAME || (config.db as any).username;
 	(config.db as any).password = process.env.APP_DB_PASSWORD || (config.db as any).password;
-	(config.db as any).port = parseInt(process.env.APP_DB_PORT || "", 10) || (config.db as any).port;
+	(config.db as any).port = Number(process.env.APP_DB_PORT) || (config.db as any).port;
 	(config.db as any).host = process.env.APP_DB_HOST || (config.db as any).host;
 	(config.db as any).database = process.env.APP_DB_DATABASE || (config.db as any).database;
 	config.payment_service = process.env.APP_PAYMENT_SERVICE || config.payment_service;
@@ -53,6 +53,8 @@ export function init(filePath: string) {
 	config.commit = process.env.BUILD_COMMIT || config.commit;
 	config.timestamp = process.env.BUILD_TIMESTAMP || config.timestamp;
 	config.redis = process.env.APP_REDIS || config.redis;
+	config.statsd.host = process.env.STATSD_HOST || config.statsd.host;
+	config.statsd.port = Number(process.env.STATSD_PORT) || config.statsd.port;
 }
 
 export function getConfig<T extends Config>(): T {

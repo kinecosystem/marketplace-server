8a2c12a52c32eedb8cae8a9a3a5df602b97f003d
victor <doc999tor@gmail.com>
2019-01-13 19:25:01 +0200

    bug fix: ECO-900

--- scripts/src/models/translations.ts
+++ scripts/src/models/translations.ts
@@ -30,13 +30,13 @@ export class OfferTranslation extends BaseEntity {
 		}
 
 		const query = OfferTranslation.createQueryBuilder("translations");
-		if (languages) {
+		if (languages && !languages.length) {
 			query.where("translations.language IN (:languages)", { languages });
 		}
 		if (offerId) {
 			query.andWhere("translations.offer_id = :offerId", { offerId });
 		}
-		if (paths) {
+		if (paths && !paths.length) {
 			query.andWhere("translations.path IN (:paths)", { paths });
 		}
 		const results = await query.getMany();

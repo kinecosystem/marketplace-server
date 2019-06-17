af711ed2d48113cca9a40de2397d7a65cb5eb402
ittiel <ittiel@gmail.com>
2019-06-17 08:53:44 +0300

    adding jwt keys and debug to startup script

--- startup.sh
+++ startup.sh
@@ -6,8 +6,12 @@
 # Usage: 
 # . ./startup.sh  <PATH>
 # Example:
-# . ./startup.sh CI marketplace-public
+# . ./startup.sh ci marketplace-public
 
+if [ ${DEBUG} == "True" ]; then
+        ECHO "DEBUG"
+        set -x
+fi
 
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')
 PARAMETERS=`aws ssm --region ${REGION} get-parameters-by-path --path /${ENVIRONMENT}/marketplace/ --with-decryption --recursive`
@@ -25,5 +29,39 @@ for row in $(echo ${PARAMETERS} | jq -c '.Parameters' | jq -c '.[]'); do
     export ${KEY}=${VALUE}
 done
 
+#Special handle of JWT keys since written to files instead of environment variables
+#todo: replace with get-parameters-by-path
+#todo: consider replacing the SSM keys
 
-npm run start-merketplace-${SERVER_ROLE}
+#Get private keys
+PUBLIC_FLAG=
+DIR=jwt/private_keys
+mkdir -p $DIR
+keys=`aws --region=${REGION} ssm describe-parameters | jq -r '.Parameters[].Name' | grep "${ENVIRONMENT}/jwt/.*pem" | grep $PUBLIC_FLAG -- '-priv.pem'`
+
+# empty out current dir items
+rm -f $DIR/*.pem
+
+for key in $keys; do
+    # write the keys with the same name as the parameter name stripping off the "prod-jwt-" prefix
+    aws --region=${REGION} ssm get-parameters --names $key --with-decryption | jq -r '.Parameters[] | select(.Name == "'$key'") | .Value' > $DIR/${key:9}
+done
+
+echo wrote keys to $DIR
+
+#Repeat for public keys
+PUBLIC_FLAG=-v
+DIR=jwt/public_keys
+mkdir -p $DIR
+keys=`aws --region=${REGION} ssm describe-parameters | jq -r '.Parameters[].Name' | grep "${ENVIRONMENT}/jwt/.*pem" | grep $PUBLIC_FLAG -- '-priv.pem'`
+
+
+for key in $keys; do
+    # write the keys with the same name as the parameter name stripping off the "prod-jwt-" prefix
+    aws --region=${REGION} ssm get-parameters --names $key --with-decryption | jq -r '.Parameters[] | select(.Name == "'$key'") | .Value' > $DIR/${key:9}
+done
+
+echo wrote keys to $DIR
+
+echo "Starting: npm run start-marketplace-${SERVER_ROLE}"
+npm run start-marketplace-${SERVER_ROLE}

559cf99bc811d2273d0ba6a20a74ca9d6797880f
Shahar Solomianik <shaharsol@Shahars-MacBook-Pro.local>
2019-07-14 15:21:33 +0300

    setWatcherEndpoint will recieve blockchainVersion instead of realizing it via appId

--- scripts/src/public/services/payment.ts
+++ scripts/src/public/services/payment.ts
@@ -123,9 +123,7 @@ export async function getPayments(walletAddress: string, options?: { timeout?: n
 	return res.data;
 }
 
-export async function setWatcherEndpoint(addresses: string[], appId?: string): Promise<Watcher> {
-	const blockchainVersion = appId ? (await Application.get(appId))!.config.blockchain_version : "2";
-	// should be called from the internal server api upon creation
+export async function setWatcherEndpoint(addresses: string[], blockchainVersion: BlockchainVersion): Promise<Watcher> {
 	const payload: Watcher = { wallet_addresses: addresses, callback: webhook };
 	const res = await httpClient.put(`${ getPaymentServiceUrl(blockchainVersion) }/services/${ SERVICE_ID }`, payload);
 	return res.data;

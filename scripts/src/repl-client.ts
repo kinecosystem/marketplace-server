1f8e405fe79d7c40dcfa205f389cd83b9d71f794
Lior <liorama@users.noreply.github.com>
2019-08-14 15:11:11 +0300

    remote repl with repl client (#371)
    * remote repl with repl client
    
    * fix to remote repl log

---
+++ scripts/src/repl-client.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import * as net from "net";
+import * as path from "path";
+
+const args = process.argv.slice(2);
+if (args.length < 1 || !(args[0] as string).match(/^[\w\d.\-_]{3,}:\d{1,5}$/)) {
+	console.log(`USAGE: ${ path.basename(process.argv[1]) } <HOST:PORT>`);
+	process.exit(1);
+}
+
+const [host, port] = args[0].split(":");
+
+const socket = net.connect(Number(port), host);
+
+process.stdin.pipe(socket);
+socket.pipe(process.stdout);
+
+socket.on("connect", () => {
+	process.stdin.setRawMode!(true);
+});
+
+socket.on("close", () => {
+	console.log("\nSocket closed");
+	process.exit(0);
+});
+
+process.on("exit", () => {
+	console.log("Exiting...");
+	socket.end();
+});

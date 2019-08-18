29d36aa627448088485054632391ea9eb2e4836a
Shahar Solomianik <shaharsol@Shahars-MacBook-Pro.local>
2019-08-18 16:46:02 +0300

    remove file

--- scripts/src/ptbr.ts
+++
@@ -1,2 +0,0 @@
-import * as adaptTranslations from "./adapt_translation_csv";
-adaptTranslations.processFile("data/translations/pt-BR.csv", "/Users/shaharsol/git/kin/csvplayground/out.csv", "/Users/shaharsol/git/kin/csvplayground/ptbr.csv");

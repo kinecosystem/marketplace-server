9e2181ed2959a7c3a146bfb43c9bcd0608da924f
Shahar Solomianik <shaharsol@Shahars-MacBook-Pro.local>
2019-09-02 17:52:07 +0300

    increased daily cap

--- data/offers/disocver_kin_app.sql
+++ data/offers/disocver_kin_app.sql
@@ -16,13 +16,13 @@ insert into offer_contents(id,offer_id,content,content_type) values('wuUYOyCvK6v
 insert into offer_contents(id,offer_id,content,content_type) values('yfnaqKTU6F4vSuvMNAGd','Oxhstb0IDrFCFOARcIr2q','"{\"pages\":[{\"type\":5,\"image\":\"\",\"title\":\"Discover Kin Apps\",\"iframeSrc\":\"https://www.kin.org/kin-apps/?utm_source=ecosystem&utm_medium=p365\"}]}"','poll');
 insert into offer_contents(id,offer_id,content,content_type) values('84GLa5w5VLkxa4t6AdUa','OQ5Bspwnnun7AeW2WxbA6','"{\"pages\":[{\"type\":5,\"image\":\"\",\"title\":\"Discover Kin Apps\",\"iframeSrc\":\"https://www.kin.org/kin-apps/?utm_source=ecosystem&utm_medium=kik\"}]}"','poll');
 
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('mkme','OQ5Bspwnnun7AeW2WxbA6','{"total":1000,"per_user":1}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('gphy','Oyew4TIXcU61KdUk4e1FU','{"total":1000,"per_user":1}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('lipz','OYnzlJyGn2uTIm1hyYiLk','{"total":1000,"per_user":1}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('tapa','OEENWYuf6sZfozKEFgL6J','{"total":1000,"per_user":1}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('swel','Og80ugJSsulScN0LlLv1Y','{"total":1000,"per_user":1}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('p365','Oxhstb0IDrFCFOARcIr2q','{"total":1000,"per_user":1}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('kik','OQ5Bspwnnun7AeW2WxbA6','{"total":1000,"per_user":1}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('mkme','OQ5Bspwnnun7AeW2WxbA6','{"total":1000,"per_user":5}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('gphy','Oyew4TIXcU61KdUk4e1FU','{"total":1000,"per_user":5}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('lipz','OYnzlJyGn2uTIm1hyYiLk','{"total":1000,"per_user":5}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('tapa','OEENWYuf6sZfozKEFgL6J','{"total":1000,"per_user":5}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('swel','Og80ugJSsulScN0LlLv1Y','{"total":1000,"per_user":5}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('p365','Oxhstb0IDrFCFOARcIr2q','{"total":1000,"per_user":5}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('kik','OQ5Bspwnnun7AeW2WxbA6','{"total":1000,"per_user":5}','GDNJRI53DAO63JXQE2COPUDE3B3B6V5GGIIGE5QE4VZQZ5S2EHXHMZ6E',100000);
 
 # test
 insert into offers(id,created_date,name,amount,meta,type,owner_id) values('OQ5Bspwnnun7AeW2WxbA6',now(),'Discover Kin Apps smpl',1,'{"title":"Discover Kin Apps","image":"https://cdn.kinecosystem.com/thumbnails/offers/discover_kin_icon_orig.png","description":"Earn and use your Kin across many apps","order_meta":{"title":"Bad Offer","description":"Bad Offer"}}','earn','UWNw1teevBjVlhwA2uXc');
@@ -31,5 +31,5 @@ insert into offers(id,created_date,name,amount,meta,type,owner_id) values('Oyew4
 insert into offer_contents(id,offer_id,content,content_type) values('rzyUkTBS5N5Za0GvAgc4','OQ5Bspwnnun7AeW2WxbA6','"{\"pages\":[{\"type\":5,\"image\":\"\",\"title\":\"Discover Kin Apps\",\"iframeSrc\":\"https://www.kin.org/kin-apps/?utm_source=ecosystem&utm_medium=smpl\"}]}"','poll');
 insert into offer_contents(id,offer_id,content,content_type) values('G1GJjKvWoxCTn6F9B59V','Oyew4TIXcU61KdUk4e1FU','"{\"pages\":[{\"type\":5,\"image\":\"\",\"title\":\"Discover Kin Apps\",\"iframeSrc\":\"https://www.kin.org/kin-apps/?utm_source=ecosystem&utm_medium=test\"}]}"','poll');
 
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('smpl','OQ5Bspwnnun7AeW2WxbA6','{"total":1000,"per_user":1}','GBMYGQBX5HH66VNF5UQHFTSUUKADWOESCVRAGM55GDHGJNFGKBD2SJED',100000);
-insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('test','Oyew4TIXcU61KdUk4e1FU','{"total":1000,"per_user":1}','GBMYGQBX5HH66VNF5UQHFTSUUKADWOESCVRAGM55GDHGJNFGKBD2SJED',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('smpl','OQ5Bspwnnun7AeW2WxbA6','{"total":1000,"per_user":5}','GBMYGQBX5HH66VNF5UQHFTSUUKADWOESCVRAGM55GDHGJNFGKBD2SJED',100000);
+insert into application_offers(app_id,offer_id,cap,wallet_address,sort_index) values('test','Oyew4TIXcU61KdUk4e1FU','{"total":1000,"per_user":5}','GBMYGQBX5HH66VNF5UQHFTSUUKADWOESCVRAGM55GDHGJNFGKBD2SJED',100000);
